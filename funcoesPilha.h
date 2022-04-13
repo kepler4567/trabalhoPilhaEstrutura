2e9d9585203b22399dd7d94800859382a9fa4fb0
Kepler Batista <estudo.batista456@gmail.com>
2022-04-13 15:41:15 -0300

    Primeiro commit

---
+++ funcoesPilha.h
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "pilhaEstatica.h"
+
+typedef struct pilha
+{
+    int pilha[MAX];
+    int topo;
+} PILHA;
+
+
+int pilhaCheia(PILHA p)
+{
+    return (p.topo == MAX - 1);
+}
+
+int pilhaVazia(PILHA p)
+{
+    return (p.topo == BASE);
+}
+
+void inicializaPilha(PILHA *p)
+{
+    p->topo = BASE;
+}
+
+int empilha(PILHA *p, int n)
+{
+    if (pilhaCheia(*p))
+        return 0;
+    else
+    {
+        p->topo++;
+        p->pilha[p->topo] = n;
+        return 1;
+    }
+}
+
+int desempilha(PILHA *p, int *n)
+{
+    if (pilhaVazia(*p))
+        return 0;
+    else
+    {
+        *n = p->pilha[p->topo];
+        p->topo--;
+        return 0;
+    }
+}
+
+int elementoDoTopo(PILHA p)
+{
+    return(p.pilha[p.topo]);
+}
